8553361b1026a7bba2395efd67fdb2b159ea20a9
standardforver <standard.forver123@gmail.com>
2023-03-07 18:09:16 -0500

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,11 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require file
         """
-        return (False)
+        if (path is None or excluded_paths is None or excluded_paths == []):
+            return (True)
+        elif (path in excluded_paths or path + '/' in excluded_paths):
+            return (False)
+        return (True)
 
     def authorization_header(self, request=None) -> str:
         """ Responsible for authorization_header
