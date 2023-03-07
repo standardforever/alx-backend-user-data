3047c5a5d709081ef6866e10235bb05cca8c5483
standardforver <standard.forver123@gmail.com>
2023-03-07 18:11:31 -0500

    update

---
+++ 0x01-Basic_authentication1/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Creating An authentication class in Flask
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Authentication Class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require file
+        """
+        if (path is None or excluded_paths is None or excluded_paths == []):
+            return (True)
+        elif (path in excluded_paths or path + '/' in excluded_paths):
+            return (False)
+        return (True)
+
+    def authorization_header(self, request=None) -> str:
+        """ Responsible for authorization_header
+        """
+        authorization = request.__dict__.get("headers").get("Authorization")
+        if (request is None or authorization is None):
+            return (None)
+        return (authorization)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ It Returns the current User
+        """
+        return (None)
