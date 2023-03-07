a3674acd4fb8c3e12956860b9bdf4962616e3c09
standardforver <standard.forver123@gmail.com>
2023-03-07 17:56:40 -0500

    before request handle fo authentication headers

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,29 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+
+if (getenv("AUTH_TYPE") == "auth"):
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request_handler():
+    """ Handles authentication and authentication headers
+    """
+    if (auth is not None):
+        print(request.path)
+        auth_paths = auth.require_auth(request.path, ['/api/v1/status/',
+                                                      '/api/v1/unauthorized/',
+                                                      '/api/v1/forbidden/'])
+        if (auth_paths is True):
+            if (auth.authorization_header(request) is None):
+                abort(401)
+
+            if (auth.current_user(request) is None):
+                abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,12 +12,19 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require file
         """
-        return (False)
+        if (path is None or excluded_paths is None or excluded_paths == []):
+            return (True)
+        elif (path in excluded_paths or path + '/' in excluded_paths):
+            return (False)
+        return (True)
 
     def authorization_header(self, request=None) -> str:
         """ Responsible for authorization_header
         """
-        return (None)
+        authorization = request.__dict__.get("headers").get("Authorization")
+        if (request is None or authorization is None):
+            return (None)
+        return (authorization)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ It Returns the current User
